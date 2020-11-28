5afbd43597f3acd1d680270753dc7a56a13f0cfc
Bruce Perens <bruce@perens.com>
2020-11-27 23:39:44 -0800

    Use &block arguments.

--- src/delegate.cr
+++ src/delegate.cr
@@ -1,43 +1,32 @@
 class Object
   macro delegate2(name, to)
-    {% methods = @type.methods %}
-    {% got_name = false %}
-    {% for m in methods %}
-      {% if m.name.id == name.id %}
-        {% got_name = true %}
-        {% has_yield = false %}
-        {% if m.body.class_name == "Expressions" %}
-          {% for node in m.body.expressions %}
-            {% if node.class_name == "Yield" %}
-              {% has_yield = true %}
-            {% end %}
-          {% end %}
-        {% end %}
-        {% if m.body.class_name == "Yield" %}
-          {% has_yield = true %}
-        {% end %}
-        def {{name}}({{m.args.join(",").id}}) {{ m.return_type.stringify != "" ? ": #{m.return_type}".id : "".id }}
-          {{to}}.{{name}}({{m.args.map{ |arg| arg.internal_name }.join(", ").id}}) {{has_yield ? "do |*args| yield *args end".id : "".id}}
-        end
-      {% end %}
-    {% end %}
-    {% if !got_name %}
-      {% raise "delegate: Method \"#{name}\" wasn't found in \"#{@type.name}\"." %}
-    {% end %}
-    {% debug %}
+   {% if m = @type.methods.find &.name.id.==(name.id) %}
+     {% if m.block_arg %}
+       {% args = m.block_arg.restriction.inputs %}
+       {% yield_args = args.map_with_index{|a,index| "arg#{index}".id}.splat %}
+     {% else %}
+       {% yield_args = "*args".id %}
+     {% end %}
+     def {{name}}({{(m.args + ["&#{m.block_arg}".id]).splat}}) {% if !m.return_type.is_a?(Nop) %}: {{m.return_type}} {% end %}
+       {{to}}.{{name}}({{m.args.map(&.internal_name).splat}}) {% if m.accepts_block? %}{ |{{yield_args}}| yield({{yield_args}})}{% end %}
+     end
+   {% else %}
+     {% raise "delegate: Method \"#{name}\" wasn't found in \"#{@type.name}\"." %}
+   {% end %}
+   {% debug %}
   end
 end
 
 class A
-  def foo(a : Int32) : String
-    yield
+  def foo(a : Int32, &block : Int32 -> String) : String
+    yield a
   end
 end
 
 class B
   @a = A.new
-  A.delegate2(foo, to: @a)
+  A.delegate2 foo, to: @a
 end
 
 b = B.new
-p b.foo(1) { "Hello" }
+p b.foo(1) { |i| "Hello" }
